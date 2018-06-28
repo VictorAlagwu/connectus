5dfd59b522e175e4bbb050cfdd3f0411f5455528
victoralagwu <victoralagwu@gmail.com>
2018-06-28 14:09:22 +0100

    Modification

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8">
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $thread->title }}</div>
+                <div class="panel-heading">{{$thread->title}}</div>
                 <div class="panel-body">
                     <article>
-                        {{ $thread->body }}
+                        {{$thread->body}}
                     </article>
                 </div>
             </div>
